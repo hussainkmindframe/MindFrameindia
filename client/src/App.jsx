19ddbb55966f0d46cec7a1e6cfbfe81e638eed35
JmartGit <jerusalemmarketint@gmail.com>
2026-05-12 12:18:07 +0530

    fix scrolltotop case

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -17,7 +17,7 @@ import { useAuthStore } from "./utils/authStore";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import ProtectedRoute from "./components/ProtectedRoute";
-import scrolltotop from "./components/scrolltotop"; 
+// import scrolltotop from "./components/scrolltotop"; 
 
 import Home from "./pages/Home";
 import Blogs from "./pages/Blogs";
@@ -79,7 +79,7 @@ export default function App() {
 
   return (
     <Router>
-      <scrolltotop/> {/* ← ADD THIS */}
+      {/* <scrolltotop/> ← ADD THIS */}
       <Toaster
         position="top-right"
         toastOptions={{
